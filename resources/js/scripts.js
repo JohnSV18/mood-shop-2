6c4d084e0eabac3112b064bfaca77e770e98476b
John Sag <johnsag@Johns-MBP-2.lan1>
2020-10-07 16:09:32 -0400

    displayed moods with b utton using javascript

---
+++ resources/js/scripts.js
@@ -0,0 +1,41 @@
+import data from './data.js'
+
+const itemsContainer = document.getElementById('items')
+
+for (let i=0; i<data.length; ++i) {
+    //create a new div element and give it a class name
+    let newDiv = document.createElement('div');
+    newDiv.className = 'item'
+
+    //create an image element
+    let img = document.createElement('img');
+    // this will change each time we go through the loop. Can you explain why?
+    img.src = data[i].image
+    img.width = 300
+    img.height = 300
+    newDiv.appendChild(img)
+
+    let desc = document.createElement('P')
+    //give the paragraph text from the data
+    desc.innerText = data[i].desc
+    // append the paragprah to the div
+    newDiv.appendChild(desc)
+    //do the same thing for price
+    let price = document.createElement('P')
+    price.innerText = data[i].price
+    newDiv.appendChild(price)
+
+    let button = document.createElement('button')
+    button.id = data[i].name
+
+    button.dataset.price = data[i].price
+    button.innerHTML = "Add to cart"
+    newDiv.appendChild(button)
+    //put new div inside items container
+
+
+
+    //add the image to the div
+    itemsContainer.appendChild(newDiv)
+    console.log(img)
+}
