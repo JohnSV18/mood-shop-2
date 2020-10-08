b5767931120f9f5846aa9c8180ff2280558985c4
John Sag <johnsag@Johns-MacBook-Pro-2.local>
2020-10-07 21:18:43 -0400

    add button connected to cart

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -47,6 +47,12 @@ for (let i=0; i<data.length; ++i) {
     itemsContainer.appendChild(newDiv)
     console.log(img)
 }
+const all_items_button = Array.from(document.querySelectorAll("button"))
+all_items_button.forEach(elt => elt.addEventListener('click', () => {
+    addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
+    showItems()
+  }))
+  
 
 const cart = []
 
