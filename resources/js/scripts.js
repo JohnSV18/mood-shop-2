f4d06791701b688cd565b1d737451f1dbc382afb
John Sag <johnsag@Johns-MacBook-Pro-2.local>
2020-10-08 01:35:49 -0400

    cart fully functional

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -6,6 +6,8 @@ const itemList = document.getElementById('item-list')
 const cartQty = document.getElementById('cart-qty')
 const cartTotal = document.getElementById('cart-total')
 
+const cart = []
+
 itemList.innerHTML = '<li>Hello World</li>'
 
 console.log(itemList)
@@ -52,10 +54,52 @@ all_items_button.forEach(elt => elt.addEventListener('click', () => {
     addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
     showItems()
   }))
+
+item_list.onchange = function(e) {
+    if(e.target && e.target.classList.contains('update')){
+       const name = e.target.dataset.name
+       const qty = parseInt(e.target.value)
+       updateCart(name, qty)
+
+    }
+
+}
+
+function updateCart(name,qty) {
+    for(let i = 0; i < cart.length; i += 1) {
+        if (cart[i].name === name) {
+            if (qty < 1) {
+                removeItem(name)
+                return
+            }
+            cart[i].qty = qty
+            
+            showItems()
+
+            return
+        }
+
+    }
+}
+
+itemList.onclick = function(e) {
+   // console.log("clicked list!")
+   // console.log(e.target)
+   if (e.target && e.target.classList.contains('remove')){
+       const name = e.target.dataset.name
+       removeItem(name)
+   } else if (e.target && e.target.classList.contains('add-one')){
+       const name = e.target.dataset.name
+       addItem(name)
+   } else if (e.target && e.target.classList.contains('remove-one')) {
+       const name = e.target.dataset.name
+       removeItem(name,1)
+   }
+
   
 
-const cart = []
 
+}
 //------------------------------------------------------
 // add items funciton
 
@@ -88,7 +132,12 @@ function showItems() {
 
       const { name, price, qty } = cart[i]
 
-      itemStr += `<li> ${name} $${price} x ${qty} = ${qty * price}</li>`
+      itemStr += `<li> ${name} $${price} x ${qty} = ${qty * price}
+      <button class = "remove" data-name = "${name}">Remove</button>
+      <button class = "add-one" data-name = "${name}"> + </button>
+      <button class = "remove-one" data-name = "${name}"> - </button>
+      <input class = "update" type = "number" data-name = ${name}>
+      </li>`
     }
     itemList.innerHTML = itemStr
     
@@ -122,7 +171,7 @@ function removeItem(name, qty = 0) {
             if (cart[i].qty < 1 || qty === 0){
                 cart.splice(i, 1)
             }
-            
+            showItems()
             return 
         }
     }
